305b463448781c64975b9c40189af07aea8e7a85
Josef Fröhle <github@josef-froehle.de>
2024-12-22 20:54:28 +0100

    add napi versions and get artifacts for release

--- build-linux.sh
+++ build-linux.sh
@@ -16,4 +16,6 @@ npm install
 # Führe prebuildify mit den gewünschten Optionen aus
 # npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=false --tag-uv --tag-armv --tag-libc --strip --arch ia32
 npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=false --tag-uv --tag-armv --tag-libc --strip --arch x64
+npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=true --tag-uv --tag-armv --tag-libc --strip --arch x64
 npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=false --tag-uv --tag-armv --tag-libc --strip --arch arm64
+npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=true --tag-uv --tag-armv --tag-libc --strip --arch arm64
