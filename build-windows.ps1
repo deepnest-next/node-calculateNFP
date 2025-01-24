c3178c8fb5801b9b84c70758a0f838242c53324b
Fxllo <dallagliofilippo32@gmail.com>
2025-01-24 11:21:45 +0100

    optimized build

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,14 +1,3 @@
 Remove-Item -ErrorAction SilentlyContinue -Recurse -Force .\node_modules\, .\build\, .\prebuilds\, .\package-lock.json
-
-# Hol die aktuelle Version von Node.js
-$NodeVersion = &node -v
-# Setze sie in eine Umgebungsvariable
-[System.Environment]::SetEnvironmentVariable('NODE_VERSION', $NodeVersion, [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('NODE_VERSION', "20.18.1", [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('ELECTRON_VERSION', "32.2.7", [System.EnvironmentVariableTarget]::Process)
-
 npm install
-
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=false --tag-uv --tag-armv --tag-libc --strip --arch ia32
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=false --tag-uv --tag-armv --tag-libc --strip --arch x64
-#npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=false --tag-uv --tag-armv --tag-libc --strip --arch arm64
+node-gyp rebuild
