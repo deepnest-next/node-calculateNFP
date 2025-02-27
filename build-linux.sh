b4575be2b883278f8bcfa5ed2ad9afcc430335e6
Josef Froehle <github@josef-froehle.de>
2025-02-27 15:54:03 +0100

    Update support electron v34 and node v22

--- build-linux.sh
+++ build-linux.sh
@@ -7,15 +7,17 @@ rm -rf ./node_modules/ ./build/ ./prebuilds/ ./package-lock.json
 NODE_VERSION=$(node -v)
 # Setze die Umgebungsvariablen
 export NODE_VERSION="$NODE_VERSION"
-export NODE_VERSION="20.18.1"
+export NODE_VERSION2="20.18.1"
+export NODE_VERSION3="22.14.0"
 export ELECTRON_VERSION="32.2.7"
+export ELECTRON_VERSION2="34.3.0"
 
 # Installiere die Abhängigkeiten
 npm install
 
 # Führe prebuildify mit den gewünschten Optionen aus
 # npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=false --tag-uv --tag-armv --tag-libc --strip --arch ia32
-npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=false --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch x64
-npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=true --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch x64
-npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=false --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch arm64
-npx prebuildify -t "$NODE_VERSION" -t "electron@$ELECTRON_VERSION" --napi=true --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch arm64
+npx prebuildify -t "$NODE_VERSION" -t "$NODE_VERSION2" -t "$NODE_VERSION3" -t "electron@$ELECTRON_VERSION" -t "electron@$ELECTRON_VERSION2" --napi=false --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch x64
+npx prebuildify -t "$NODE_VERSION" -t "$NODE_VERSION2" -t "$NODE_VERSION3" -t "electron@$ELECTRON_VERSION" -t "electron@$ELECTRON_VERSION2" --napi=true --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch x64
+npx prebuildify -t "$NODE_VERSION" -t "$NODE_VERSION2" -t "$NODE_VERSION3" -t "electron@$ELECTRON_VERSION" -t "electron@$ELECTRON_VERSION2" --napi=false --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch arm64
+npx prebuildify -t "$NODE_VERSION" -t "$NODE_VERSION2" -t "$NODE_VERSION3" -t "electron@$ELECTRON_VERSION" -t "electron@$ELECTRON_VERSION2" --napi=true --tag-uv --tag-armv --tag-libc --strip -n calculateNfp --arch arm64
