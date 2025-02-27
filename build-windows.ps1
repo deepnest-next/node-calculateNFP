d2a19ba04965ed1d450afa223f6e465929e294c8
Josef Froehle <github@josef-froehle.de>
2025-02-27 15:54:03 +0100

    Update support electron v34 and node v22

--- build-windows.ps1
+++ build-windows.ps1
@@ -7,14 +7,20 @@ Remove-Item -ErrorAction SilentlyContinue -Recurse -Force .\node_modules\, .\bui
 $NodeVersion = &node -v
 # Setze sie in eine Umgebungsvariable
 [System.Environment]::SetEnvironmentVariable('NODE_VERSION', $NodeVersion, [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('NODE_VERSION', "20.18.1", [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable('ELECTRON_VERSION', "32.2.7", [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable('NODE_VERSION2', "20.18.1", [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable('NODE_VERSION3', "22.14.0", [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable('ELECTRON_VERSION', "32.3.1", [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable('ELECTRON_VERSION2', "34.3.0", [System.EnvironmentVariableTarget]::Process)
 
-npm install
+npm install --arch=ia32
 
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=false --tag-libc --strip -n calculateNfp --arch ia32
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=true --tag-libc --strip -n calculateNfp --arch ia32
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=false --tag-libc --strip -n calculateNfp --arch x64
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=true --tag-libc --strip -n calculateNfp --arch x64
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=false --tag-libc --strip -n calculateNfp --arch arm64
-npx prebuildify -t $env:NODE_VERSION -t electron@$env:ELECTRON_VERSION --napi=true --tag-libc --strip -n calculateNfp --arch arm64
+npx prebuildify -t $env:NODE_VERSION -t $env:NODE_VERSION2 -t $env:NODE_VERSION3 -t electron@$env:ELECTRON_VERSION -t electron@$env:ELECTRON_VERSION2 --napi=false --tag-libc --strip -n calculateNfp --arch ia32
+npx prebuildify -t $env:NODE_VERSION -t $env:NODE_VERSION2 -t $env:NODE_VERSION3 -t electron@$env:ELECTRON_VERSION -t electron@$env:ELECTRON_VERSION2 --napi=true --tag-libc --strip -n calculateNfp --arch ia32
+
+npm install --arch=x64
+npx prebuildify -t $env:NODE_VERSION -t $env:NODE_VERSION2 -t $env:NODE_VERSION3 -t electron@$env:ELECTRON_VERSION -t electron@$env:ELECTRON_VERSION2 --napi=false --tag-libc --strip -n calculateNfp --arch x64
+npx prebuildify -t $env:NODE_VERSION -t $env:NODE_VERSION2 -t $env:NODE_VERSION3 -t electron@$env:ELECTRON_VERSION -t electron@$env:ELECTRON_VERSION2 --napi=true --tag-libc --strip -n calculateNfp --arch x64
+
+npm install --arch=arm64
+npx prebuildify -t $env:NODE_VERSION -t $env:NODE_VERSION2 -t $env:NODE_VERSION3 -t electron@$env:ELECTRON_VERSION -t electron@$env:ELECTRON_VERSION2 --napi=false --tag-libc --strip -n calculateNfp --arch arm64
+npx prebuildify -t $env:NODE_VERSION -t $env:NODE_VERSION2 -t $env:NODE_VERSION3 -t electron@$env:ELECTRON_VERSION -t electron@$env:ELECTRON_VERSION2 --napi=true --tag-libc --strip -n calculateNfp --arch arm64
